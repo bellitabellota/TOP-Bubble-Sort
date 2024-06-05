aac07cc50fd47f1d2e0fff229236e1c1971696c5
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-06-05 14:26:16 +0100

    Check if all value[index] are smaller than value[index + 1] otherwise repeat

--- script.rb
+++ script.rb
@@ -1,15 +1,29 @@
 def bubble_sort (array)
-  sorting_array = nil
+  sorting_array = "not assigned yet"
 
+  # circle through array once and switch if value[index] is bigger than value[index + 1]
   array.each_with_index do |number, index|
 
     if number == array[-1]
-      next
+      break
     elsif number > array[index + 1]
       array[index] = array[index + 1]
       array[index + 1] = number
     end
+
     sorting_array = array
+  end
+
+  # check if in sorting_array all value[index] are smaller than value[index + 1]
+  # if not repeat previous step
+
+  sorting_array.each_with_index do |value, index|
+    if value == sorting_array[-1]
+      next
+    elsif value <= sorting_array[index + 1]
+    else
+      bubble_sort(sorting_array)
+    end
   end
 end
 
